68cb6452948beefc3252fcce33453d4cc044bbea
sudo5in5k <sudo5in5k@gmail.com>
2020-05-30 23:03:27 +0900

    add binds factory

--- app/src/main/java/com/example/daggerintentsample/di/ViewModelModule.kt
+++ app/src/main/java/com/example/daggerintentsample/di/ViewModelModule.kt
@@ -1,11 +1,13 @@
 package com.example.daggerintentsample.di
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.daggerintentsample.viewmodel.FirstViewModel
 import com.example.daggerintentsample.viewmodel.SecondViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
+import javax.inject.Singleton
 
 @Module
 abstract class ViewModelModule {
@@ -21,4 +23,8 @@ abstract class ViewModelModule {
     @ViewModelKey(SecondViewModel::class)
     @ActivityScope
     abstract fun bindSecondViewModel(viewModel: SecondViewModel): ViewModel
+
+    @Binds
+    @ActivityScope
+    internal abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 }
